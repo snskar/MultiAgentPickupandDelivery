290506330390c390a4c7ae9d960ae8179790f28c
snskar <80286267+snskar@users.noreply.github.com>
2021-03-28 22:17:33 +0530

    Add files via upload

---
+++ sourcecode/single_agent_planner.py
@@ -0,0 +1,153 @@
+import heapq
+
+
+def move(loc, dir):
+    directions = [(0, -1), (1, 0), (0, 1), (-1, 0), (0,0)]
+    return loc[0] + directions[dir][0], loc[1] + directions[dir][1]
+
+
+def get_sum_of_cost(paths):
+    rst = 0
+    for path in paths:
+        rst += len(path) - 1
+    return rst
+
+
+def compute_heuristics(my_map, goal):
+    # Use Dijkstra to build a shortest-path tree rooted at the goal location
+    open_list = []
+    closed_list = dict()
+    root = {'loc': goal, 'cost': 0}
+    heapq.heappush(open_list, (root['cost'], goal, root))
+    closed_list[goal] = root
+    while len(open_list) > 0:
+        (cost, loc, curr) = heapq.heappop(open_list)
+        for dir in range(4):
+            child_loc = move(loc, dir)
+            child_cost = cost + 1
+            if child_loc[0] < 0 or child_loc[0] >= len(my_map) \
+                    or child_loc[1] < 0 or child_loc[1] >= len(my_map[0]):
+                continue
+            if my_map[child_loc[0]][child_loc[1]]:
+                continue
+            child = {'loc': child_loc, 'cost': child_cost}
+            if child_loc in closed_list:
+                existing_node = closed_list[child_loc]
+                if existing_node['cost'] > child_cost:
+                    closed_list[child_loc] = child
+                    # open_list.delete((existing_node['cost'], existing_node['loc'], existing_node))
+                    heapq.heappush(open_list, (child_cost, child_loc, child))
+            else:
+                closed_list[child_loc] = child
+                heapq.heappush(open_list, (child_cost, child_loc, child))
+
+    # build the heuristics table
+    h_values = dict()
+    for loc, node in closed_list.items():
+        h_values[loc] = node['cost']
+    return h_values
+
+
+def build_constraint_table(constraints, agent):
+    constraint_table = {}
+    for constraint in constraints:
+        if constraint['timestep'] not in constraint_table.keys():
+            constraint_table[constraint['timestep']] = []
+        constraint_table[constraint['timestep']].append(constraint['loc'])
+
+    return constraint_table
+    pass
+
+
+def get_location(path, time):
+    if time < 0:
+        return path[0]
+    elif time < len(path):
+        return path[time]
+    else:
+        return path[-1]  # wait at the goal location
+
+
+def get_path(goal_node):
+    path = []
+    curr = goal_node
+    while curr is not None:
+        path.append(curr['loc'])
+        curr = curr['parent']
+    path.reverse()
+    return path
+
+
+def is_constrained(curr_loc, next_loc, next_time, constraint_table):
+    if next_time in constraint_table.keys():
+        for location in constraint_table[next_time]:
+            if len(location) == 1:
+                if location[0] == next_loc:
+                    return True
+            if len(location) == 2:
+                if location[0] == curr_loc and location[1] == next_loc:
+                    return True
+        return False
+    else:
+        return False
+
+    pass
+
+
+def push_node(open_list, node):
+    heapq.heappush(open_list, (node['g_val'] + node['h_val'], node['h_val'], node['loc'], node))
+
+
+def pop_node(open_list):
+    _, _, _, curr = heapq.heappop(open_list)
+    return curr
+
+
+def compare_nodes(n1, n2):
+    """Return true is n1 is better than n2."""
+    return n1['g_val'] + n1['h_val'] < n2['g_val'] + n2['h_val']
+
+
+def a_star(my_map, start_loc, goal_loc, h_values, agent, constraints):
+    """ my_map      - binary obstacle map
+        start_loc   - start position
+        goal_loc    - goal position
+        agent       - the agent that is being re-planned
+        constraints - constraints defining where robot should or cannot go at each timestep
+    """
+
+
+
+    open_list = []
+    closed_list = dict()
+    earliest_goal_timestep = 0
+    h_value = h_values[start_loc]
+    constraint_table = build_constraint_table(constraints, agent)
+    root = {'loc': start_loc, 'g_val': 0, 'h_val': h_value, 'parent': None, 'timestep': 0}
+    push_node(open_list, root)
+    closed_list[(root['loc'],root['timestep'])] = root
+    while len(open_list) > 0:
+        curr = pop_node(open_list)
+        if curr['loc'] == goal_loc:
+            return get_path(curr)
+        for dir in range(5):
+            child_loc = move(curr['loc'], dir)
+            if my_map[child_loc[0]][child_loc[1]] and not is_constrained(curr['loc'], child_loc, curr['timestep']+1, constraint_table):
+                continue
+            child = {'loc': child_loc,
+                     'g_val': curr['g_val'] + 1,
+                     'h_val': h_values[child_loc],
+                     'timestep': curr['timestep'] + 1,
+                     'parent': curr
+                     }
+
+            if (child['loc']) in closed_list:
+                existing_node = closed_list[(child['loc'])]
+                if compare_nodes(child, existing_node):
+                    closed_list[(child['loc'],child['timestep'])] = child
+                    push_node(open_list, child)
+            else:
+                closed_list[(child['loc'],child['timestep'])] = child
+                push_node(open_list, child)
+
+    return None  # Failed to find solutions
